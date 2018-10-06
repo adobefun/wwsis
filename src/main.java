afcc17778c0be4a0c48d708205457e753600f558
Kamil Kubiak <adobefun@protonmail>
2018-10-06 13:55:23 +0200

    Kamil

--- src/main.java
+++ src/main.java
@@ -1,5 +1,10 @@
 public class main {
     public static void main(String[] args) {
         System.out.println("hello");
+
+        int i = 0;
+        for(i=0; i<10; i++){
+            System.out.println(i);
+        }
     }
 }
